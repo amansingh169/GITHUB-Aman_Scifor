a8bf9977e21de844f1da03839d1faa054b121f3e
Aman Singh <as2211075@gmail.com>
2025-04-18 15:56:59 +0530

    Added Daily Task 6

---
+++ Daily Tasks/dailyTask_6.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 3;
+
+console.log("a = " + a);
+console.log("b = " + b);
+
+console.log("Addition (a + b) = " + (a + b));
+console.log("Subtraction (a - b) = " + (a - b));
+console.log("Multiplication (a * b) = " + a * b);
+console.log("Division (a / b) = " + a / b);
+console.log("Modulus (a % b) = " + (a % b));
+console.log("Exponentiation (a ** b) = " + a ** b);
